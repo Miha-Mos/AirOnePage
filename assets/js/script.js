9d9a81d1f4f32117402a06fd934bb3c3e1e12ab2
Miha-Mos <71607457+Miha-Mos@users.noreply.github.com>
2021-02-20 21:53:47 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+//Fixed Header
+
+let w = $(window).width();
+
+if (w <= 768) {
+
+ $('.header').addClass('header-fixed');
+ $('.logo-dark').addClass('logo-dark-show');
+ $('.logo-light').addClass('logo-light-hide');
+ $('.nav-title').addClass('nav-title-color');
+
+}else{
+  window.onscroll = function showHeader() {
+
+  if (window.pageYOffset > 1) {
+    $('.header').addClass('header-fixed');
+    $('.logo-dark').addClass('logo-dark-show');
+    $('.logo-light').addClass('logo-light-hide');
+    $('.nav-title').addClass('nav-title-color');
+  }else{
+    $('.header').removeClass('header-fixed');
+    $('.logo-dark').removeClass('logo-dark-show');
+    $('.logo-light').removeClass('logo-light-hide');
+    $('.nav-title').removeClass('nav-title-color');
+  };
+ };
+}; 
+
+
+//Burger Menu
+
+$(function() {
+  $('.menu-open').click(function() {
+    $('.nav-wrapper').slideToggle(),slow;
+  })
+});
+
+
+
+
+//Nav-Hover
+
+$(document).ready(function(){
+  $('.nav-title').hover(function() {
+    $(this).addClass('opacity');
+  }, function() {
+    $(this).removeClass('opacity');
+  });
+});
+
+//Carousel 
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:1,
+    loop: true,
+    autoplay:true
+  });
+});
+
+//Google Map 
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.1347265, lng: -76.7272867},
+    zoom: 6,
+    scrollwheel: false
+  });
+
+  let image = {
+    url: 'gmap-pin.png',
+    scaledSize: new google.maps.Size(45, 45)
+  };
+
+  let marker = new google.maps.Marker({
+   position: {lat: 41.1347265, lng: -76.7272867},
+   map: map,
+   title: 'Brooklyn',
+   icon: image
+  });
+}
+
+//Video 
+
+$('[data-fancybox]').fancybox({
+    youtube : {
+        controls : 1,
+        showinfo : 1
+    }
+});
+
+
